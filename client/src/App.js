3ee73fc24e582945ec319811305b5fa5467a4ecb
Olivia Lammam <olivia.l7475@gmail.com>
2022-08-04 17:03:17 -0700

    fixed issues

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 import Signup from './pages/Signup';
 import Login from './pages/Login';
@@ -11,8 +18,28 @@ import ShopMens from './pages/ShopMens';
 import ShopWomens from './pages/ShopWomens';
 import { StoreProvider } from "./utils/GlobalState";
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 function App() {
   return (
+    <ApolloProvider client={client}>
     <div>
       <div>
         <Router>
@@ -58,6 +85,7 @@ function App() {
         </Router>
       </div>
     </div>
+    </ApolloProvider>
   );
 }
 
