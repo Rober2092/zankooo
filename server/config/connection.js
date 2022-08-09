a6579c0d627eede8674980927b36e17f87190e4e
djteiji <djteiji@gmail.com>
2022-08-09 13:43:15 -0700

    dbname change

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/zankoo-test', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/Zankooo', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
