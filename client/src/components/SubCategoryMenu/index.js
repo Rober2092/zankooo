59431d40f2288d0ea41021c70714f23b7f3255e5
djteiji <djteiji@gmail.com>
2022-08-08 19:24:28 -0700

    adding css

--- client/src/components/SubCategoryMenu/index.js
+++ client/src/components/SubCategoryMenu/index.js
@@ -1,41 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { useQuery } from '@apollo/client';
-import { useStoreContext } from "../../utils/GlobalState";
-import { UPDATE_SUBCATEGORIES } from '../../utils/actions';
-import { QUERY_SUBCATEGORIES } from '../../utils/queries';
-
+import React, { useState } from 'react';
 import ProductList from '../ProductList';
 
-
 function SubCategoryMenu() {
-    const [state, dispatch] = useStoreContext();
-
-const { subcategories } = state;
-
-const { data: subcategoryData } = useQuery(QUERY_SUBCATEGORIES);
-
-useEffect(() => {
-    // if categoryData exists or has changed from the response of useQuery, then run dispatch()
-    if (subcategoryData) {
-      // execute our dispatch function with our action object indicating the type of action and the data to set our state for categories to
-      dispatch({
-        type: UPDATE_SUBCATEGORIES,
-        subcategories: subcategoryData.subcategories
-      });
-    }
-  }, [subcategoryData, dispatch]);
 
     const [currentProductList, setCurrentProductList] = useState()
     return (
         <main>
-
-            {subcategories.map((item) => (
-                <button
-                    key={item._id}
-                >
-                    {item.name}
-                </button>
-            ))}
             <ul>
                 <li>
                     <button onClick={() => setCurrentProductList(<ProductList />)}>Tops</button>
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,56 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+
 
 // 
 import CategoryMenu from '../components/CategoryMenu';
 
+
+function ProjectCard() {
+  return (
+    <Container className='container justify-content-center mt-5'>
+      
+      <Card className="card bg-dark text-white landing-card">
+      <Link to="/shopmens">
+            
+        <img className="card-img" src="/images/PopArtMan.jpg" alt="Card image" />
+        
+        <div className="card-img-overlay">
+          <h5 className="card-title">Shop Mens</h5>
+          <p className="card-text"></p>
+          <p className="card-text"></p>
+        </div>
+        </Link>
+      </Card>
+
+      <Card className="card bg-dark text-white landing-card">
+      <Link to="/shopwomens">
+        <img className="card-img" src="/images/PopArtWoman.jpg" alt="Card image" />
+        <div className="card-img-overlay">
+          <h5 className="card-title">Shop Womens</h5>
+          <p className="card-text"></p>
+          <p className="card-text"></p>
+        </div>
+        </Link>
+      </Card>
+    </Container>
+  );
+
+}
+
+
+
+
+
 const Home = () => {
   return (
     <main>
-        <CategoryMenu />
+      
+        <ProjectCard />
+
+
     </main>
   );
 };
